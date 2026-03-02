13ef33dab23661b38ff47b8563bce82f297ec07d
jakegodsall <jake.edward.godsall@gmail.com>
2026-03-02 15:17:29 +0000

    move to java 21 (want records)

--- src/main/java/com/jakegodsall/models/FindRequest.java
+++ src/main/java/com/jakegodsall/models/FindRequest.java
@@ -1,5 +1,9 @@
 package com.jakegodsall.models;
 
+import java.nio.file.Path;
+import java.util.Optional;
+import com.jakegodsall.enums.FileType;
+
 public record FindRequest(
     Path path,
     Optional<String> name,
