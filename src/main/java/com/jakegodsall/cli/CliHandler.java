3f7a3f81fc1e707a2aec102ac72c9228ad5b7947
jakegodsall <jake.edward.godsall@gmail.com>
2026-03-02 15:20:23 +0000

    cli handler wip

--- src/main/java/com/jakegodsall/cli/CliHandler.java
+++ src/main/java/com/jakegodsall/cli/CliHandler.java
@@ -9,40 +9,21 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import com.jakegodsall.models.FindRequest;
+
 public class CliHandler {
-    private class CliResult {
-        private final Path path;
-        private final String name;
-        private final char type;
-        private final int maxDepth;
-        private final boolean hidden;
-        private final boolean print0;
-
-        public CliResult(
-            Path path, String name, char type, int maxDepth, boolean hidden, boolean print0
-        ) {
-            this.path = path;
-            this.name = name;
-            this.type = type;
-            this.maxDepth = maxDepth;
-            this.hidden = hidden;
-            this.print0 = print0;
-        }
-    }
-
     public void parse(String[] args) {
         CommandLineParser parser = new DefaultParser();
         
         try {
             CommandLine cli = parser.parse(getOptions(), args);
-
-            
+            FindRequest result = validate(cli);
         } catch (ParseException ex) {
             System.out.println("Error parsing the provided arguments: " + ex.getMessage());
         }
     }
 
-    private CliResult validate(CommandLine cli) {
+    private FindRequest validate(CommandLine cli) {
 
         String[] remainder = cli.getArgs();
         if (remainder.length != 1) {
@@ -53,7 +34,7 @@ public class CliHandler {
         String name = cli.getOptionValue("name");
         
         String typeRaw = cli.getOptionValue("type");
-        if (typeRaw.toLowerCase() !== "f" || typeRaw.toLowerCase() !== "d") {
+        if (!typeRaw.toLowerCase().equals("f") && !typeRaw.toLowerCase().equals("d")) {
             throw new IllegalArgumentException("The type must be either f or d");
         }
         char type = Character.toLowerCase(typeRaw.charAt(0));
@@ -66,7 +47,7 @@ public class CliHandler {
         boolean hidden = cli.hasOption("hidden");
         boolean print0 = cli.hasOption("print0");
 
-        return new CliResult(path, name, type, maxDepth, hidden, print0);
+        return new FindRequest(path, name, type, maxDepth, hidden, print0);
     }
 
     private Options getOptions() {
