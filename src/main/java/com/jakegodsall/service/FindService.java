a03feb3fc27a92a869a33e29f20d6620a7997e42
jakegodsall <jake.edward.godsall@gmail.com>
2026-03-10 10:13:09 +0000

    add listAll method on the FindService

--- src/main/java/com/jakegodsall/service/FindService.java
+++ src/main/java/com/jakegodsall/service/FindService.java
@@ -1,13 +1,20 @@
 package com.jakegodsall.service;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import com.jakegodsall.models.FindRequest;
 
 public class FindService {
     public static void listAll(FindRequest request) {
         Path p = request.path();
-        System.out.println(Files.isDirectory(p));
+
+        try (Stream<Path> paths = Files.list(p)) {
+            paths.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println("Error listing objects in path: " + p);
+        }
     }
 }
